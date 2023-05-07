e578356ef85e09ed3af4bb9afcd2f61734c8f51c
Bohan Hou <64535834+bohanhou14@users.noreply.github.com>
2023-05-07 16:07:17 -0400

    Add files via upload

--- get_text.py
+++ get_text.py
@@ -9,18 +9,13 @@ def get_text(url):
     html = res.read().decode('utf-8')
     soup = BeautifulSoup(html, 'html.parser')
     texts = []
-    for text in soup.find_all('p'):
-        text = remove_links(text)
-        text = " ".join(text)
-        texts.append(text)
+    # print(soup.get_text())
+    for item in soup.find_all('p', class_ = ""):
+        texts.append(item.get_text(strip = True))
     texts = " ".join(texts)
     texts = texts.replace("\n", " ")
-    texts = " ".join(texts.split(" "))
     return texts
         
-# remove the links in the list and preserve the texts
-def remove_links(data):
-    return [elem if str(elem)[0] != "<" and elem[0] != ">" else "" for elem in data]
 def main():
     site = sys.argv[1]
     text = get_text(site)
